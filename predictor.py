9d8c2cf65d3cccce2eb8adcb98fca3f5ce32142c
heamgadiyaram <heam.gadiyaram07@gmail.com>
2025-08-12 00:09:39 -0400

    calculated offensive features to add to model

--- predictor.py
+++ predictor.py
@@ -65,11 +65,59 @@ merged_scores = play_data_2024.copy()
 for col in ['Visitor', 'VisitorScore', 'Home', 'HomeScore']:
     merged_scores[col] = merge_visitor[col].combine_first(merge_home[col])
 
-merged_scores['Homewon'] = merged_scores['HomeScore'] > merged_scores['VisitorScore']
+merged_scores['HomeWon'] = merged_scores['HomeScore'] > merged_scores['VisitorScore']
 
 nfl_schedule = pd.read_csv('nfl_schedule.csv')
 nfl_schedule['Home'] = nfl_schedule['Home'].map(team_mapping)
 nfl_schedule['Away'] = nfl_schedule['Away'].map(team_mapping)
+
+avg_points_scored_home = merged_scores.groupby('Home')['HomeScore'].mean()
+avg_points_scored_visitor = merged_scores.groupby('Visitor')['VisitorScore'].mean()
+
+avg_points_allowed_home = merged_scores.groupby('Home')['VisitorScore'].mean()
+avg_points_allowed_visitor = merged_scores.groupby('Visitor')['HomeScore'].mean()
+
+#calculate avg points
+overall_avg_points_scored = (avg_points_scored_home + avg_points_scored_visitor) / 2
+overall_avg_points_allowed = (avg_points_allowed_home + avg_points_allowed_visitor) / 2
+
+#total wins
+home_wins = merged_scores.groupby('Home')['HomeWon'].sum()
+visitor_wins = merged_scores.groupby('Visitor').apply(lambda x: len(x) - x['HomeWon'].sum())
+total_games_home = merged_scores['Home'].value_counts()
+total_games_visitor = merged_scores['Visitor'].value_counts()
+
+#calc win percentage
+overall_wins = home_wins + visitor_wins
+total_games = total_games_home + total_games_visitor
+total_wins = overall_wins / total_games
+
+print(merged_scores.columns)
+#calculate yards per play
+avg_yards_per_play_home = merged_scores.groupby('Home')['Yards'].mean()
+avg_yards_per_play_visitor = merged_scores.groupby('Visitor'['Yards']).mean()
+
+#calculate yards per game
+avg_yards_per_game_home = merged_scores.groupby(['SeasonYear', 'Home'])['Yards'].mean()
+avg_yards_per_game_visitor = merged_scores.groupby(['SeasonYear', 'Visitor'])['Yards'].mean()
+overall_avg_yards_per_game = (avg_yards_per_game_home + avg_yards_per_game_visitor) / 2
+
+#calculate average pass completion rate
+avg_pass_completion_rate_home = merged_scores.groupby('Home').apply(lambda x: 1 - x['IsIncomplete'].mean())
+avg_pass_completion_rate_visitor = merged_scores.groupby('Visitor').apply(lambda x: 1 - x['IsIncomplete'].mean())
+overall_avg_pass_completion = (avg_pass_completion_rate_home + avg_yards_per_game_visitor) / 2
+
+#calculate touchdowns per game
+avg_touchdowns_per_game_home = merged_scores.groupby(['SeasonYear', 'Home'])['IsTouchdown'].mean()
+avg_touchdowns_per_game_visitor = merged_scores.groupby(['SeasonYear', 'Home'])['IsTouchdown'].mean()
+overall_avg_touchdowns_per_game = (avg_touchdowns_per_game_home + avg_touchdowns_per_game_visitor) / 2
+
+#calculate rush success rate
+avg_rush_yards_home = merged_scores.groupby('Home').apply(lambda x: x['Yards'][x['IsRush'] == 1].mean())
+avg_rush_yards_visitor = merged_scores.groupby('Visitor').apply(lambda x: x['Yards'][x['IsRush'] == 1].mean())
+overall_avg_rush_yards = (avg_rush_yards_home + avg_rush_yards_visitor) / 2
+
+
 
 
 
